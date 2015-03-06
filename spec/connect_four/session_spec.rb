f1cb76d8ad2913da6be5f2e5745ec09d662550a3
Jason Hopper <spectre6000@gmail.com>
2015-03-06 10:04:09 -0700

    tied the rest of the spec files to their files being tested

--- spec/connect_four/session_spec.rb
+++ spec/connect_four/session_spec.rb
@@ -1,45 +1,47 @@
-# describe Session do
-# 	session = Session.new
-# 	player1 = Player.new("Bob", "X")
-# 	player2 = Player.new("Joe", "O")
-# 	interface = Interface.new(session)
-# 	game = Game.new(player1, player2, interface)
-# 	board = Board.new(game)
+require 'connect_four'
 
-# 	describe "#new" do
-# 		xit "creates an instance of Session" do
-# 			expect(session).to be_an_instance_of(Session)
-# 		end
-# 	end
+describe Session do
+	session = Session.new
+	player1 = Player.new("Bob", "X")
+	player2 = Player.new("Joe", "O")
+	interface = Interface.new(session)
+	game = Game.new(player1, player2, interface)
+	board = Board.new(game)
 
-# 	describe "#setup" do
-# 		xit "creates player1" do
-# 			expect(session.player1).to be_an_instance_of(Player)
-# 		end
+	describe "#new" do
+		xit "creates an instance of Session" do
+			expect(session).to be_an_instance_of(Session)
+		end
+	end
 
-# 		xit "creates player2" do
-# 			expect(session.player2).to be_an_instance_of(Player)
-# 		end
+	describe "#setup" do
+		xit "creates player1" do
+			expect(session.player1).to be_an_instance_of(Player)
+		end
 
-# 		xit "gets a high score" do
-# 			allow(interface).to receive(:get_high_score).and_return(3)
-# 			expect(session.high_score).to eql(2)
-# 		end
-# 	end
+		xit "creates player2" do
+			expect(session.player2).to be_an_instance_of(Player)
+		end
 
-# 	describe "#play" do
+		xit "gets a high score" do
+			allow(interface).to receive(:get_high_score).and_return(3)
+			expect(session.high_score).to eql(2)
+		end
+	end
 
-# 		xit "creates a game if neither player has won" do
-# 			allow(player1).to receive(:score).and_return(0)
-# 			allow(interface).to receive(:get_high_score).and_return(3)
-# 			expect(session.games.length).to eql(1)
-# 		end
+	describe "#play" do
 
-# 		xit "does not create a game if either player has won" do
-# 			allow(player1).to receive(:score).and_return(2)
-# 			allow(interface).to receive(:get_high_score).and_return(3)
-# 			expect(session.games.length).to eql(0)
-# 		end
-# 	end
-# end
+		xit "creates a game if neither player has won" do
+			allow(player1).to receive(:score).and_return(0)
+			allow(interface).to receive(:get_high_score).and_return(3)
+			expect(session.games.length).to eql(1)
+		end
+
+		xit "does not create a game if either player has won" do
+			allow(player1).to receive(:score).and_return(2)
+			allow(interface).to receive(:get_high_score).and_return(3)
+			expect(session.games.length).to eql(0)
+		end
+	end
+end
 
