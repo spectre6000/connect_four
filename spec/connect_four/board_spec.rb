459a1138236d1f584312de9be70402b4b24bd70e
Jason Hopper <spectre6000@gmail.com>
2015-03-06 11:54:51 -0700

    got board spec to pass

--- spec/connect_four/board_spec.rb
+++ spec/connect_four/board_spec.rb
@@ -1,48 +1,63 @@
 require 'connect_four'
 
 describe Board do
-	session = Session.new
-	player1 = Player.new("Bob", "X")
-	player2 = Player.new("Joe", "O")
-	interface = Interface.new(session)
-	game = Game.new(player1, player2, interface)
-	subject {Board.new(game)}
+	let(:player1) {double("Player", :mark => "X")}
+	let(:player2) {double("Player", :mark => "O")}
+	let(:game) {double("Game", :player1 => "player1", :player2 => "player2")}
+	let(:board) {Board.new(game)}
+	# session = Session.new
+	# player1 = Player.new("Bob", "X")
+	# player2 = Player.new("Joe", "O")
+	# interface = Interface.new(session)
+	# game = Game.new(player1, player2, interface)
+	# subject {Board.new(game)}
 
 	describe "#new" do
 		it "creates an instance of Board" do
-			expect(subject).to be_an_instance_of(Board)
+			expect(board).to be_an_instance_of(Board)
 		end
-		xit "creates a grid of 42 spaces" do
-			expect(subject.spaces.length).to eql(42)
+		it "creates a grid of 42 spaces" do
+			expect(board.spaces.length).to eql(42)
 		end
 	end
 	describe "#win?" do
-		xit "throws true for vertical win" do
-			subject.move(1,"X")
-			subject.move(1,"X")
-			subject.move(1,"X")
-			subject.move(1,"X")
-			expect(subject.win?).to eql(true)
+		it "throws false for no win" do
+			board.move(1, player1)
+			board.move(1, player2)
+			board.move(1, player1)
+			board.move(1, player2)
+			board.win?
+			expect(board.win).to eql(false)
 		end
-		xit "throws true for horizontal win" do
-			subject.move(1,"X")
-			subject.move(2,"X")
-			subject.move(3,"X")
-			subject.move(4,"X")
-			expect(subject.win?).to eql(true)
+		it "throws true for vertical win" do
+			board.move(1, player1)
+			board.move(1, player1)
+			board.move(1, player1)
+			board.move(1, player1)
+			board.win?
+			expect(board.win).to eql(true)
 		end
-		xit "throws true for diagonal win" do
-			subject.move(1,"X")
-			subject.move(1,"O")
-			subject.move(1,"X")
-			subject.move(1,"O")
-			subject.move(1,"O")
-			subject.move(1,"X")
-			subject.move(1,"O")
-			subject.move(1,"O")
-			subject.move(1,"O")
-			subject.move(1,"X")
-			expect(subject.win?).to eql(true)
+		it "throws true for horizontal win" do
+			board.move(1, player1)
+			board.move(2, player1)
+			board.move(3, player1)
+			board.move(4, player1)
+			board.win?
+			expect(board.win).to eql(true)
+		end
+		it "throws true for diagonal win" do
+			board.move(1, player1)
+			board.move(2, player2)
+			board.move(2, player1)
+			board.move(3, player2)
+			board.move(3, player2)
+			board.move(3, player1)
+			board.move(4, player2)
+			board.move(4, player2)
+			board.move(4, player2)
+			board.move(4, player1)
+			board.win?
+			expect(board.win).to eql(true)
 		end
 	end
 end
