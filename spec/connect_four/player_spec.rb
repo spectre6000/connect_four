f551fe6192ed94bf3087f8bc6372a883aae3f83f
Jason Hopper <spectre6000@gmail.com>
2015-03-06 10:07:03 -0700

    player spec specs no longer pending, all passing

--- spec/connect_four/player_spec.rb
+++ spec/connect_four/player_spec.rb
@@ -4,19 +4,19 @@ describe Player do
 	subject {Player.new("Bob", "X")}
 
 	describe "#new" do
-		xit "creates an instance of Player" do
+		it "creates an instance of Player" do
 			expect(subject).to be_an_instance_of(Player)
 		end
 
-		xit "properly sets the name attribute" do
+		it "properly sets the name attribute" do
 			expect(subject.name).to eql("Bob")
 		end
 
-		xit "properly sets the mark attribute" do
+		it "properly sets the mark attribute" do
 			expect(subject.mark).to eql("X")
 		end
 
-		xit "properly sets the score attribute" do
+		it "properly sets the score attribute" do
 			expect(subject.score).to eql(0)
 		end
 	end
