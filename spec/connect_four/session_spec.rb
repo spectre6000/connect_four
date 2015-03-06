321ce081839a72681f84c1fc0c6e2bc45f9714d2
Jason Hopper <spectre6000@gmail.com>
2015-03-06 10:13:44 -0700

    got a few tests to work in session spec

--- spec/connect_four/session_spec.rb
+++ spec/connect_four/session_spec.rb
@@ -1,30 +1,31 @@
 require 'connect_four'
 
 describe Session do
+	let (:interface) {double("Interface")}
 	session = Session.new
-	player1 = Player.new("Bob", "X")
-	player2 = Player.new("Joe", "O")
-	interface = Interface.new(session)
-	game = Game.new(player1, player2, interface)
-	board = Board.new(game)
+	# player1 = Player.new("Bob", "X")
+	# player2 = Player.new("Joe", "O")
+	# interface = Interface.new(session, fake_stdout)
+	# game = Game.new(player1, player2, interface)
+	# board = Board.new(game)
 
 	describe "#new" do
-		xit "creates an instance of Session" do
+		it "creates an instance of Session" do
 			expect(session).to be_an_instance_of(Session)
 		end
 	end
 
 	describe "#setup" do
-		xit "creates player1" do
+		it "creates player1" do
 			expect(session.player1).to be_an_instance_of(Player)
 		end
 
-		xit "creates player2" do
+		it "creates player2" do
 			expect(session.player2).to be_an_instance_of(Player)
 		end
 
 		xit "gets a high score" do
-			allow(interface).to receive(:get_high_score).and_return(3)
+			allow(:interface).to receive(:get_high_score).and_return(3)
 			expect(session.high_score).to eql(2)
 		end
 	end
@@ -32,8 +33,8 @@ describe Session do
 	describe "#play" do
 
 		xit "creates a game if neither player has won" do
-			allow(player1).to receive(:score).and_return(0)
-			allow(interface).to receive(:get_high_score).and_return(3)
+			allow(session.player1).to receive(:score).and_return(0)
+			allow(:interface).to receive(:get_high_score).and_return(3)
 			expect(session.games.length).to eql(1)
 		end
 
