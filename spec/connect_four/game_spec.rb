029aaa04ac19c0f1b8e5a08dd2b9bfb97f852f09
Jason Hopper <spectre6000@gmail.com>
2015-03-06 20:28:28 -0700

    trying again with the spaces...

--- spec/connect_four/game_spec.rb
+++ spec/connect_four/game_spec.rb
@@ -1,61 +1,60 @@
 require 'connect_four'
 
 describe Game do
-	let (:fake_stdout) {double("stdout")}
-	let (:player1) {double("Player")}
-	let (:player2) {double("Player")}
-	let (:interface) {double("Interface", :out => "fake_stdout")}
-	let (:game) {Game.new(player1, player2, interface)}
-	# session = Session.new
-	# player1 = Player.new("Bob", "X")
-	# player2 = Player.new("Joe", "O")
-	# interface = Interface.new(session)
-	# game = Game.new(player1, player2, interface)
-	# board = Board.new(game)
+  let (:fake_stdout) {double("stdout")}
+  let (:player1) {double("Player")}
+  let (:player2) {double("Player")}
+  let (:interface) {double("Interface", :out => "fake_stdout")}
+  let (:game) {Game.new(player1, player2, interface)}
+  # session = Session.new
+  # player1 = Player.new("Bob", "X")
+  # player2 = Player.new("Joe", "O")
+  # interface = Interface.new(session)
+  # game = Game.new(player1, player2, interface)
+  # board = Board.new(game)
 
-	describe "#new" do
-		it "creates an instance of Game" do
-			allow(interface).to receive(:puts)
-			expect(game).to be_an_instance_of(Game)
-		end
-	# xit "selects a player to go first" do
-	# 	allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("HEADS")
-	# 	expect(game.turn).to be_an_instance_of(Player)
-	# end
-	# xit "gets correct player from matching coin toss" do
-	# 	srand(1) # player2 calls; coin flips to TAILS
-	# 	allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("TAILS")
-	# 	# expect([player1, player2]).to include(subject.turn)
-	# 	expect(game.turn).to eql(player2)
-	# end
-	# xit "gets correct player from missed coin toss" do
-	# 	srand(1) # player2 calls; coin flips to TAILS
-	# 	allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("HEADS")
-	# 	expect(game.turn).to eql(player1)
-	# end
-
-	# describe "#play" do
-	# 	xit "correctly places a piece in the first column" do
-	# 	allow_any_instance_of(Interface).to receive(:move).and_return(1)
-	# 	board.move(1,"X")
-	# 	expect(["X"]).to include(board.spaces[0][1])
-	# 	end
-	# 	xit "correctly places a piece in the second column" do
-	# 	allow_any_instance_of(Interface).to receive(:move).and_return(1)
-	# 	board.move(2,"O")
-	# 	expect(["O"]).to include(board.spaces[6][1])
-	# 	end
-	# 	xit "correctly places a second piece in the first column" do
-	# 	allow_any_instance_of(Interface).to receive(:move).and_return(1)
-	# 	board.move(1,"O")
-	# 	expect(["O"]).to include(board.spaces[1][1])
-	# 	end
-	# 	xit "takes turns" do
-	# 	game.turn = player1
-	# 	game.change_player
-	# 	expect(game.turn).to eql(player2)
-	# 	end
-	# end
-	end
+  describe "#new" do
+    it "creates an instance of Game" do
+      allow(interface).to receive(:puts)
+      expect(game).to be_an_instance_of(Game)
+    end
+  #   xit "selects a player to go first" do
+  #     allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("HEADS")
+  #     expect(game.turn).to be_an_instance_of(Player)
+  #   end
+  #   xit "gets correct player from matching coin toss" do
+  #     srand(1) # player2 calls; coin flips to TAILS
+  #     allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("TAILS")
+  #     # expect([player1, player2]).to include(subject.turn)
+  #     expect(game.turn).to eql(player2)
+  #   end
+  #   xit "gets correct player from missed coin toss" do
+  #     srand(1) # player2 calls; coin flips to TAILS
+  #     allow_any_instance_of(Interface).to receive(:get_coin_call).and_return("HEADS")
+  #     expect(game.turn).to eql(player1)
+  #   end
+  end
+  # describe "#play" do
+  #   xit "correctly places a piece in the first column" do
+  #     allow_any_instance_of(Interface).to receive(:move).and_return(1)
+  #     board.move(1,"X")
+  #     expect(["X"]).to include(board.spaces[0][1])
+  #   end
+  #   xit "correctly places a piece in the second column" do
+  #     allow_any_instance_of(Interface).to receive(:move).and_return(1)
+  #     board.move(2,"O")
+  #     expect(["O"]).to include(board.spaces[6][1])
+  #   end
+  #   xit "correctly places a second piece in the first column" do
+  #     allow_any_instance_of(Interface).to receive(:move).and_return(1)
+  #     board.move(1,"O")
+  #     expect(["O"]).to include(board.spaces[1][1])
+  #   end
+  #   xit "takes turns" do
+  #     game.turn = player1
+  #     game.change_player
+  #     expect(game.turn).to eql(player2)
+  #   end
+  # end
 end
 
--- spec/connect_four/player_spec.rb
+++ spec/connect_four/player_spec.rb
@@ -1,24 +1,24 @@
 require 'connect_four'
 
 describe Player do
-	subject {Player.new("Bob", "X")}
+  subject {Player.new("Bob", "X")}
 
-	describe "#new" do
-		it "creates an instance of Player" do
-			expect(subject).to be_an_instance_of(Player)
-		end
+  describe "#new" do
+    it "creates an instance of Player" do
+      expect(subject).to be_an_instance_of(Player)
+    end
 
-		it "properly sets the name attribute" do
-			expect(subject.name).to eql("Bob")
-		end
+    it "properly sets the name attribute" do
+      expect(subject.name).to eql("Bob")
+    end
 
-		it "properly sets the mark attribute" do
-			expect(subject.mark).to eql("X")
-		end
+    it "properly sets the mark attribute" do
+      expect(subject.mark).to eql("X")
+    end
 
-		it "properly sets the score attribute" do
-			expect(subject.score).to eql(0)
-		end
-	end
+    it "properly sets the score attribute" do
+      expect(subject.score).to eql(0)
+    end
+  end
 end
 
