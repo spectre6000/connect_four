b615e8748596764ee7930b475825beee05412a84
Eno Compton 4 <eno4@ecom.com>
2015-03-06 20:32:46 -0700

    Refactor board and spec
    Use `private` keyword to indicate which methods are truly private.
    
    Move initialization code into `initialize`. It shouldn't be in a
    separate method. If an object requires anything more than trivial
    initialization, the object is doing way to much and needs to be
    simplified.
    
    Use standard code formatting.
    
    Remove comments from spec. They don't add anything to the code and are a
    distraction. By using git, whatever changes you have are always in the
    history. If you want to return to some older version of the code, the
    history is always there to help you get back there.
    
    Also, define `winners` as a constant rather than a method. Since the
    `winners` array doesn't ever change, it's a perfect use for a constant.
    By using a method that returns an array, you have a situation where Ruby
    will make a new array every time you refer to `winners`. Granted, this
    isn't a huge problem, but it's still better as a constant.

--- spec/connect_four/board_spec.rb
+++ spec/connect_four/board_spec.rb
@@ -1,25 +1,21 @@
 require 'connect_four'
 
 describe Board do
-  let(:player1) {double("Player", :mark => "X")}
-  let(:player2) {double("Player", :mark => "O")}
-  let(:game) {double("Game", :player1 => "player1", :player2 => "player2")}
-  let(:board) {Board.new(game)}
-  # session = Session.new
-  # player1 = Player.new("Bob", "X")
-  # player2 = Player.new("Joe", "O")
-  # interface = Interface.new(session)
-  # game = Game.new(player1, player2, interface)
-  # subject {Board.new(game)}
+  let(:player1) { double("Player", :mark => "X")}
+  let(:player2) { double("Player", :mark => "O")}
+  let(:game) { double("Game", :player1 => "player1", :player2 => "player2") }
+  let(:board) { Board.new(game) }
 
   describe "#new" do
     it "creates an instance of Board" do
       expect(board).to be_an_instance_of(Board)
     end
+
     it "creates a grid of 42 spaces" do
       expect(board.spaces.length).to eql(42)
     end
   end
+
   describe "#win?" do
     it "throws false for no win" do
       board.move(1, player1)
@@ -29,6 +25,7 @@ describe Board do
       board.win?
       expect(board.win).to eql(false)
     end
+
     it "throws true for vertical win" do
       board.move(1, player1)
       board.move(1, player1)
@@ -37,6 +34,7 @@ describe Board do
       board.win?
       expect(board.win).to eql(true)
     end
+
     it "throws true for horizontal win" do
       board.move(1, player1)
       board.move(2, player1)
@@ -45,6 +43,7 @@ describe Board do
       board.win?
       expect(board.win).to eql(true)
     end
+
     it "throws true for diagonal win" do
       board.move(1, player1)
       board.move(2, player2)
@@ -57,6 +56,7 @@ describe Board do
       board.move(4, player2)
       board.move(4, player1)
       board.win?
+
       expect(board.win).to eql(true)
     end
   end
