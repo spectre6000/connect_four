447e5e8f6dc2b34d9b19a04a68ea63c7073a2248
Jason Hopper <spectre6000@gmail.com>
2015-03-10 11:12:50 -0600

    finally making some progress

--- spec/connect_four/interface_spec.rb
+++ spec/connect_four/interface_spec.rb
@@ -1,9 +1,10 @@
 require 'connect_four'
 
 describe Interface do
-  let (:fake_stdout)  {double("STDOUT")}
+  let (:fake_stdout) {double("$STDOUT")}
   let (:player1) {instance_double("Player", :name => "Bob")}
   let (:session) {instance_double("Session")}
+  let (:board) {instance_double("Board")}
   let (:interface) { Interface.new(session, fake_stdout) }
 
 
@@ -22,38 +23,40 @@ describe Interface do
 
   describe "#get_coin_call" do
     it "prompts players to answer HEADS or TAILS" do
-      expect{fake_stdout}.to receive(:puts).with("\nBob, heads or tails?")
+      expect(fake_stdout).to receive(:puts).with("\nBob, heads or tails?")
       interface.get_coin_call(player1)
-      # expect{fake_stdout}.to receive(:puts).with("\nBob, heads or tails?")
     end
   end
 
-  # describe "#get_high score" do
-  #   xit "prompts players to enter the number of rounds" do
-  #     expect{interface.get_high_score}.to output("Best out of how many?\n").to_stdout
-  #   end
-  # end
+  describe "#get_high score" do
+    it "prompts players to enter the number of rounds" do
+      expect(fake_stdout).to receive(:puts).with("Best out of how many?")
+      interface.get_high_score
+    end
+  end
 
-  # describe "#move" do
-  #   xit "prompts player for where to place a piece" do
-  #     expect{interface.move(player1.name)}.to output("Bob, where would you like to place your piece?\n").to_stdout
-  #   end
-  # end
+  describe "#move" do
+    it "prompts player for where to place a piece" do
+      expect(fake_stdout).to receive(:puts).with("Bob, where would you like to place your piece?\n")
+      interface.move(player1.name)
+    end
+  end
 
-  # describe "#print_board" do
-  #   xit "displays the game board" do
-  #     # player2 = Player.new("Joe", "O")
-  #     game = Game.new(player1, player2, interface)
-  #     board = Board.new(game)
-  #     expect{interface.print_board(board)}.to output("| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n").to_stdout
-  #   end
-  # end
+  describe "#print_board" do
+    xit "displays the game board" do
+      allow(board).to receive(:spaces).and_return(" ")
+      expect(fake_stdout).to receive(:puts).with("| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n")
+      interface.print_board(board)
+    end
+  end
 
-  # describe "#winner" do
-  #   xit "displays the winner" do
-  #     expect{interface.winner(player1)}.to output("Bob wins!\n").to_stdout
-  #   end
-  # end
+  describe "#winner" do
+    it "displays the winner" do
+      allow(player1).to receive(:score).and_return(4)
+      expect(fake_stdout).to receive(:puts).with("Bob wins!")
+      interface.winner(player1)
+    end
+  end
 
 end
 
